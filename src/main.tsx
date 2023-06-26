4a88565956ac96a2fce897407c655fec93ea7ea8
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-06-26 16:39:03 -0300

    homework 1/2

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './cleanCode/04-homework.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
