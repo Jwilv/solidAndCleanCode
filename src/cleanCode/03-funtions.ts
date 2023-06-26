3a5187c548c103947f630c9224af9c2d61142a98
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-06-26 15:14:01 -0300

    getPayAmount

--- src/cleanCode/03-funtions.ts
+++ src/cleanCode/03-funtions.ts
@@ -39,4 +39,40 @@
         return true;        
 
     }
+
+
+    /////////////// original ///////////////////
+
+    // const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+    //     let result;
+    //     if ( isDead ) {
+    //         result = 1500;
+    //     } else {
+    //         if ( isSeparated ) {
+    //             result = 2500;
+    //         } else {
+    //             if ( isRetired ) {
+    //                 result = 3000;
+    //             } else {
+    //                 result = 4000; 
+    //             }
+    //         }
+    //     }
+        
+    //     return result;
+    // }
+
+    ///////////////////     refactor       ///////////////////////////////
+
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+
+        if ( isDead ) return 1500;
+
+        if ( isSeparated ) return 2500;
+
+        return ( isRetired ) ? 3000 : 4000
+
+    }
+
 })();
