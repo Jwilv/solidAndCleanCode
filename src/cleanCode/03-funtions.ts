33e2b6d308bd7ff25aba5840ab61d8e73fb35e79
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-06-26 15:11:04 -0300

    03-funtions

---
+++ src/cleanCode/03-funtions.ts
@@ -0,0 +1,42 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBioById( id: string ) {
+        console.log({ id });
+    }
+
+    interface MovieProps {
+        title : string;
+        description : string;
+        rating : number;
+        cast : string[];
+    }
+    
+    // Crear una película
+    function createMovie({title, description, rating, cast} : MovieProps ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+})();
