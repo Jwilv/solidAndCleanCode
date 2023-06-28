ca67cb4cc10fb479ca521c0f207b7bca6f29937d
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-06-28 14:36:52 -0300

    dry

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './cleanCode/04-homework.ts'
+import './cleanCode/05-dry.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
