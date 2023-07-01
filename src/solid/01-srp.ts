25ac8cd56ed5928c5205a73fba704ccbc2b94124
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-07-01 19:10:06 -0300

    con srp

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -1,41 +1,78 @@
-( ()=>{
+(() => {
 
     interface Product {
-        id : number;
-        name : string;
+        id: number;
+        name: string;
     }
 
     //Usualmente,esto es una clase para controlar la vista desplegada
     //Recuerden que podemos tener muchas vistas que realicen este mismo trabajo 
 
-    class ProductBloc{
+    class ProductService {
 
-        loadProduct( id : number){
+        loadProduct(id: number) {
             //Realiza un proceso para obtener el producto y retornarlo 
-            console.log( 'Producto :', {id, name : 'Oled tv'})
+            console.log('Producto :', { id, name: 'Oled tv' })
         }
 
-        saveProduct( product : Product ){
+        saveProduct(product: Product) {
             //guarda el producto en la base de datos
             console.log('producto guardado en la base de datos', product)
         }
 
-        notofyClients(){
+    }
+
+    class Mailer {
+
+        sendEmails() {
             console.log('enviando correo a los clientes')
         }
 
-        onAddToCart( productId : number ){
-            //agregar al carrito de compras 
+    }
+
+    class ProductBloc {
+
+        private productService : ProductService;
+        private mailer : Mailer; 
+
+        constructor( productSerice : ProductService, mailer : Mailer){
+            this.productService = productSerice;
+            this.mailer = mailer;             
+        }
+
+        loadProduct(id: number) {
+            //Realiza un proceso para obtener el producto y retornarlo 
+            this.productService.loadProduct( id )
+        }
+
+        saveProduct(product: Product) {
+            //guarda el producto en la base de datos
+            this.productService.saveProduct( product)
+        }
+
+        notofyClients() {
+            this.mailer.sendEmails()
+        }
+
+    }
+
+    class ProductCart{
+
+        addToCart( productId : number ){
             console.log('Agregando al carrito', productId)
         }
     }
 
-    const productBoc = new ProductBloc()
+    const productService = new ProductService()
+    const mailer = new Mailer()
+
+    const productBoc = new ProductBloc( productService, mailer)
+    const productCart = new ProductCart()
 
     productBoc.loadProduct(10)
-    productBoc.saveProduct({id:3,name:'pc'})
+    productBoc.saveProduct({ id: 3, name: 'pc' })
     productBoc.notofyClients()
-    productBoc.onAddToCart( 10 )
+    productCart.addToCart(33)
 
 
 
