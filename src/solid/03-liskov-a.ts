27de83cc12c329f5d6ff1b4b18a2353db71f5f88
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-07-02 13:55:24 -0300

    solucion

--- src/solid/03-liskov-a.ts
+++ src/solid/03-liskov-a.ts
@@ -1,33 +1,13 @@
-import { Audi, Honda, Tesla, Toyota } from "./03-liskov-b";
+import { Audi, Honda, Tesla, Toyota, Vehicle } from "./03-liskov-b";
 
 
 (() => {
 
-    const printCarSeats = (cars: (Tesla | Audi | Toyota | Honda)[]) => {
+    const printCarSeats = (cars: Vehicle[]) => {
 
-        for (const car of cars) {
-
-            if (car instanceof Tesla) {
-                console.log('tesla', car.getNumberOfTeslaSeats())
-                continue;
-            }
-
-            if (car instanceof Audi) {
-                console.log('Audi', car.getNumberOfAudiSeats())
-                continue;
-            }
-
-            if (car instanceof Toyota) {
-                console.log('Toyota', car.getNumberOfToyotaSeats())
-                continue;
-            }
-
-            if (car instanceof Honda) {
-                console.log('Honda', car.getNumberOfHondaSeats())
-                continue;
-            }
-
-        }
+        cars.forEach( car =>{
+            console.log( car.constructor.name, car.getNumberOfSeats() )
+        })
 
     }
 
